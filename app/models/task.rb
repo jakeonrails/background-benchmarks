4292534752a46c3e0a0d45c8053fe836bdd2f996
Jake Moffatt <jakeonrails@gmail.com>
2012-05-30 20:28:40 -0700

    Add simple Task class to test some "work" for each type of queue

---
+++ app/models/task.rb
@@ -0,0 +1,11 @@
+class Task
+
+  def self.perform(one, two, three)
+    Rails.logger.info("#{one} #{two} #{three}")
+  end
+
+  def perform(one, two, three)
+    Rails.logger.info("#{one} #{two} #{three}")
+  end
+
+end
