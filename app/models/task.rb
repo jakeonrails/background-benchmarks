79539fc98a6fa6e22982de6551e796134e47f4a2
Jake Moffatt <jakeonrails@gmail.com>
2012-05-30 21:59:14 -0700

    Make Task perform instance method work without args

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,17 @@
 class Task
-
   @queue = 'task'
 
   def self.perform(one, two, three)
     Rails.logger.info("#{Time.now.to_f} #{one} #{two} #{three}")
   end
 
-  def perform(one, two, three)
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one, @two, @three = one, two, three
+  end
+
+  def perform
     Rails.logger.info("#{Time.now.to_f} #{one} #{two} #{three}")
   end
 
